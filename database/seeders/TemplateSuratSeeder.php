456f6eb667c2c06c196748864b8a01e01ef60ec0
diberkha <diberkhasp@student.uns.ac.id>
2025-12-17 11:02:16 +0700

    [UPDATE] Template SK Direktur

--- database/seeders/TemplateSuratSeeder.php
+++ database/seeders/TemplateSuratSeeder.php
@@ -9,6 +9,15 @@ class TemplateSuratSeeder extends Seeder
 {
     public function run(): void
     {
-        
+        $templates = [
+            ['nama_template_surat' => 'Surat Keputusan Direktur'],
+        ];
+
+        foreach ($templates as $tpl) {
+            TemplateSurat::firstOrCreate(
+                ['nama_template_surat' => $tpl['nama_template_surat']],
+                $tpl
+            );
+        }
     }
 }
